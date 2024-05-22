b24a2930913199004d9499952c3c2d62a4dd0001
Elangovan <Elangovan@DESKTOP-S50B1AL>
2024-05-22 19:33:47 +0530

    initial commit

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,10 +1,36 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+	static int score;
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Main obj = new Main();
+		List<char[][]> shape = new ArrayList<>();
+		obj.loadShapes(shape);
 		
+		boolean flag = true;
+		do {
+		} while (flag);
 
+		sc.close();
+
+	}
+
+	private void loadShapes(List<char[][]> shape) {
+		shape.add(new char[][] { { '#', '#' }, { '#', '#' } });
+
+		shape.add(new char[][] { { '#', '#', '#', '#' } });
+
+		shape.add(new char[][] { { '#', ' ' }, { '#', ' ' }, { '#', '#' } });
+
+		shape.add(new char[][] { { '#', '#', ' ' }, { ' ', '#', '#' } });
+
+		shape.add(new char[][] { { ' ', '#', ' ' }, { '#', '#', '#' } });
 	}
 
 }
